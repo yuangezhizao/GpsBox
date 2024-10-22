d28a36423b6a296c02821cc6eca6c44775085fe6
yuangezhizao-M1Pro <yuangezhizao@icloud.com>
2024-10-23 00:51:11 +0800

    ✨ Feat(gpsbox.ino): 解析 GPS 数据并发送至 USB 串口

--- GpsBox.ino
+++ GpsBox.ino
@@ -16,6 +16,89 @@ void setup() {
   Serial.println("[" + String(millis()) + "] Setup() end, start loop()...");
 }
 
+void print_GPS_data(TinyGPSPlus& gps) {
+  char hdop_str[16];
+  char satellites_str[16];
+
+  char time_str[20];
+  char date_str[20];
+  char location_str[30];
+  char location_age_str[16];
+  char altitude_str[16];
+  char course_str[16];
+  char speed_str[16];
+
+  if (gps.hdop.isValid()) {
+    sprintf(hdop_str, "HDOP: %.f", gps.hdop.hdop());
+  } else {
+    sprintf(hdop_str, "HDOP: -");
+  }
+
+  if (gps.satellites.isValid()) {
+    sprintf(satellites_str, "Sat: %d", gps.satellites.value());
+  } else {
+    sprintf(satellites_str, "Sat: *");
+  }
+
+  if (gps.time.isValid()) {  //TODO: bypass 00:00:00
+    // sprintf(time_str, "Time: %02d:%02d:%02d.%02d",
+    //         gps.time.hour(), gps.time.minute(), gps.time.second(), gps.time.centisecond());
+    sprintf(time_str, " %02d:%02d:%02d",
+            gps.time.hour(), gps.time.minute(), gps.time.second());
+  } else {
+    sprintf(time_str, "INV");
+  }
+
+  if (gps.date.isValid()) {
+    sprintf(date_str, "D/T: %02d/%02d/%02d",
+            gps.date.year(), gps.date.month(), gps.date.day());
+  } else {
+    sprintf(date_str, "D/T: INV");
+  }
+
+  if (gps.location.isValid()) {
+    sprintf(location_str, "Loc: %.6f %.6f", gps.location.lat(), gps.location.lng());
+    sprintf(location_age_str, "Age: %d", gps.location.age());
+  } else {
+    sprintf(location_str, "Loc: INV");
+    sprintf(location_age_str, "Age: INV");
+  }
+
+  if (gps.altitude.isValid()) {
+    sprintf(altitude_str, "Alt: %.2f", gps.altitude.meters());
+  } else {
+    sprintf(altitude_str, "Alt: INV");
+  }
+
+  if (gps.course.isValid()) {
+    sprintf(course_str, "Cou: %.2f", gps.course.deg());
+  } else {
+    sprintf(course_str, "Cou: INV");
+  }
+
+  if (gps.speed.isValid()) {
+    // sprintf(speed_str, "%.2f km/h", gps.speed.kmph());
+    sprintf(speed_str, "%.2f", gps.speed.kmph());
+  } else {
+    sprintf(speed_str, "Spd: INV");
+  }
+
+  print_to_serial(hdop_str, satellites_str, time_str, date_str, location_str, location_age_str, altitude_str, course_str, speed_str);
+}
+
+void print_to_serial(const char* hdop_str, const char* satellites_str, const char* time_str, const char* date_str, const char* location_str, const char* location_age_str, const char* altitude_str, const char* course_str, const char* speed_str) {
+  Serial.println("\n");
+  Serial.println(hdop_str);
+  Serial.println(satellites_str);
+  Serial.print(date_str);
+  Serial.println(time_str);
+  Serial.println(location_str);
+  Serial.println(location_age_str);
+  Serial.println(altitude_str);
+  Serial.println(course_str);
+  Serial.println(speed_str);
+}
+
 void loop() {
   while (Serial0.available() > 0) {
     // Serial.write(Serial0.read());
@@ -24,6 +107,7 @@ void loop() {
     Serial.print(gps_serial);
 
     if (gps.encode(gps_serial)) {
+      print_GPS_data(gps);
       // Serial.println();
       // Serial.println("[" + String(millis()) + "] DEBUG charsProcessed: " + String(gps.charsProcessed()));
       // Serial.println("[" + String(millis()) + "] DEBUG sentencesWithFix: " + String(gps.sentencesWithFix()));
